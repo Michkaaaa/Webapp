4de482b16f7f7c47475ea2c8cd57a7779a8f8776
Michkaaaa <276875@glenbrook225.org>
2024-12-04 10:37:26 -0600

    Dec-4

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
-function show(){      
-    document.getElementById("panel-1").style.display = "none";
-    document.getElementById("panel-2").style.display = "block";
- }
- 
- function hide(){
-    document.getElementById("panel-1").style.display = "block";
-    document.getElementById("panel-2").style.display = "none";
- }
+function show(){
+   document.getElementById("panel-1").style.display = "none";
+   document.getElementById("panel-2").style.display = "block";
+}
+
+function hide(){
+   document.getElementById("panel-1").style.display = "block";
+   document.getElementById("panel-2").style.display = "none";
+}
+
